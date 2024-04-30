72967c618f272f600430a07a3a2602140b5abe66
iblabd <iqbalabdurrahman04@gmail.com>
2024-04-30 11:40:52 +0700

    chore: initialize tailwind css

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -9,37 +9,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex h-screen items-center justify-center bg-slate-200">
+      <h1 className="text-3xl font-bold">Tailwind works!</h1>
     </div>
   )
 }
