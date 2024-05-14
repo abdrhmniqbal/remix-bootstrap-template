c32a586166ad4cacb4be828654370b421f300111
iblabd <73406832+iblabd@users.noreply.github.com>
2024-05-14 04:50:05 +0000

    feat: add props to hero block

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { GithubIcon } from '@/assets/icons'
 import type { MetaFunction } from '@remix-run/node'
 import Header from '@/components/blocks/header'
 import Hero from '@/components/blocks/hero'
@@ -35,7 +36,26 @@ export default function Index() {
         ]}
         actionButton={<LinkButton to="/register">Get Started</LinkButton>}
       />
-      <Hero />
+      <Hero
+        title="Launch your website in days."
+        description="The boilerplate with all you need to build your production ready
+          website, and start making money online fast."
+        featuredImage="https://images.unsplash.com/photo-1556155092-490a1ba16284?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyLGxhbmRzY2FwZXx8fHx8fDE3MTUzMjMxMTA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920"
+        actionButton={
+          <LinkButton to="/login">
+            Get {getEnvValue('VITE_APP_NAME')}
+          </LinkButton>
+        }
+        secondButton={
+          <LinkButton
+            to="https://github.com/iblabd/remix-boilerplate-template"
+            target="_blank"
+          >
+            <GithubIcon className="mr-2 size-4 text-[#24292f]" />
+            View on Github
+          </LinkButton>
+        }
+      />
       <LogoList />
     </>
   )
--- src/components/blocks/hero.tsx
+++ src/components/blocks/hero.tsx
@@ -1,32 +1,40 @@
-import { GithubIcon } from '@/assets/icons'
-import { Button } from '@/components/ui/button'
+import React from 'react'
+import { cn } from '@/lib/utils'
 
-function Hero() {
-  return (
-    <section className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-16 px-8 py-8 lg:flex-row lg:items-start lg:gap-20 lg:py-20">
+interface HeroProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string
+  description: string
+  featuredImage: string
+  actionButton: JSX.Element
+  secondButton?: JSX.Element
+}
+const Hero = React.forwardRef<HTMLDivElement, HeroProps>(
+  (
+    { title, description, actionButton, secondButton, featuredImage, ...props },
+    ref,
+  ) => (
+    <section
+      className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-16 px-8 py-8 lg:flex-row lg:items-start lg:gap-20 lg:py-20"
+      ref={ref}
+      {...props}
+    >
       <div className="flex flex-col items-center justify-center gap-8 text-center lg:items-start lg:gap-10 lg:text-left">
         <h1 className="flex flex-col items-center gap-3 text-4xl font-extrabold tracking-tighter lg:items-start lg:text-6xl/tight">
-          Launch your website in days.
+          {title}
         </h1>
-        <p className="text-lg leading-relaxed opacity-80">
-          The boilerplate with all you need to build your production ready
-          website, and start making money online fast.
-        </p>
+        <p className="text-lg leading-relaxed opacity-80">{description}</p>
         <div className="flex w-full flex-col gap-4 sm:flex-row">
-          <Button
-            size="lg"
-            className="w-full sm:w-auto"
-          >
-            Get LandFast
-          </Button>
-          <Button
-            size="lg"
-            className="w-full sm:w-auto"
-            variant="outline"
-          >
-            <GithubIcon className="mr-2 size-4 text-[#24292f]" />
-            View on Github
-          </Button>
+          {actionButton &&
+            React.cloneElement(actionButton, {
+              size: actionButton.props.size ? actionButton.props.size : 'lg',
+              className: cn(actionButton.props.className, 'w-full sm:w-auto'),
+            })}
+          {secondButton &&
+            React.cloneElement(secondButton, {
+              variant: 'outline',
+              size: secondButton.props.size ? secondButton.props.size : 'lg',
+              className: cn(secondButton.props.className, 'w-full sm:w-auto'),
+            })}
         </div>
       </div>
       <div className="relative max-md:-m-4 lg:w-full">
@@ -37,11 +45,13 @@ function Hero() {
           decoding="async"
           data-nimg="1"
           className="ml-auto w-full max-w-xl rounded-md"
-          src="https://images.unsplash.com/photo-1556155092-490a1ba16284?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyLGxhbmRzY2FwZXx8fHx8fDE3MTUzMjMxMTA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920"
+          src={featuredImage}
         />
       </div>
     </section>
-  )
-}
+  ),
+)
+
+Hero.displayName = 'Hero'
 
 export default Hero
