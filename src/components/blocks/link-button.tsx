cc9e884366969f00be5a348b3deef37b7975b1e5
iblabd <73406832+iblabd@users.noreply.github.com>
2024-05-14 04:14:48 +0000

    feat: add link button block

---
+++ src/components/blocks/link-button.tsx
@@ -0,0 +1,29 @@
+import { Link } from '@remix-run/react'
+import React from 'react'
+import { Button, ButtonProps } from '@/components/ui/button'
+
+interface LinkButtonProps extends ButtonProps {
+  to: string
+  target?: React.HTMLAttributeAnchorTarget
+}
+
+export default function LinkButton({
+  to,
+  children,
+  target,
+  ...props
+}: LinkButtonProps) {
+  return (
+    <Button
+      asChild
+      {...props}
+    >
+      <Link
+        to={to}
+        target={target}
+      >
+        {children}
+      </Link>
+    </Button>
+  )
+}
