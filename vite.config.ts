7c8d4e4d6731a3c7ad0d59aee332f03c9ac671e4
iblabd <iqbalabdurrahman04@gmail.com>
2024-05-16 11:58:13 +0700

    Revert "Merge branch 'vercel' into main"
    This reverts commit 1693233671578b362d33c2688da7392f9f300749, reversing
    changes made to a10f637a4cde4cc85912abcc78938d755b205c57.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
-import { vercelPreset } from '@vercel/remix/vite'
 import { remixDevTools } from 'remix-development-tools'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
@@ -12,7 +11,6 @@ export default defineConfig({
   plugins: [
     remixDevTools(),
     remix({
-      presets: [vercelPreset()],
       appDirectory: './src/app',
       routes: async defineRoutes => {
         return flatRoutes('routes', defineRoutes, { appDir: './src/app' })
