6baa9ffb0aad539e8a2dc068c793cdf88f526b34
iblabd <iqbalabdurrahman04@gmail.com>
2024-05-26 09:33:27 +0700

    feat: use react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import { installGlobals } from '@remix-run/node'
 import { remixDevTools } from 'remix-development-tools'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
+import babel from 'vite-plugin-babel'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 installGlobals()
+
+const ReactCompilerConfig = {
+  sources: (filename: string | string[]) => {
+    return filename.indexOf('./src') !== -1
+  },
+}
 
 export default defineConfig({
   plugins: [
@@ -14,6 +21,13 @@ export default defineConfig({
       appDirectory: './src/app',
       routes: async defineRoutes => {
         return flatRoutes('routes', defineRoutes, { appDir: './src/app' })
+      },
+    }),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ['@babel/preset-typescript'],
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
       },
     }),
     tsconfigPaths(),
